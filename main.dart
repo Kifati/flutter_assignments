d853c288b15781ada0592892b151a3cf5c521779
Kifati <85504746+Kifati@users.noreply.github.com>
2023-06-26 19:44:54 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:practice1/screens/home_page.dart';
-import 'package:practice1/screens/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -11,12 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      themeMode: ThemeMode .light,
-      theme: ThemeData(primarySwatch: Colors.cyan),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      home: Material(
+        child: Container(
+          color: Colors.black,
+          alignment: Alignment.center,
+          child: Text("Hello World\nWelcome to flutter, Karan.",
+          style: TextStyle(color: Color.fromARGB(255, 77, 216, 248),
+          fontSize: 40,
+          fontWeight: FontWeight.bold
+        )
+      )
+        )
+      )
     );
   }
 }
